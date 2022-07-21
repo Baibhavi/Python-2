02295bd518386e71436a23d324fcf5a559b88e25
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-07-21 18:52:06 +0530

    Update extend nested list.py

--- Lists/extend nested list.py
+++ Lists/extend nested list.py
@@ -1,8 +1,8 @@
-#Extend nested list by adding the sublist
+'''Extend nested list by adding the sublist
 list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
 sub list to add:
 sub_list = ["h", "i", "j"]'''
-'''list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
 sub_list = ["h", "i", "j"]
 print(list1)
 list1[2][1][2].extend(sub_list)
