1392f34c3991aa047ff265f99d890eceacf27c38
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-07-21 18:45:00 +0530

    Create concatenate2.py

---
+++ Lists/concatenate2.py
@@ -0,0 +1,9 @@
+#Concatenate 2 lists in following order:
+list1=["Hello", "take"]
+list2=["Dear","Sir']
+output:
+list3=['Hello Dear','Hello Sir','take Dear','take Sir']'''
+'''list1=["Hello", "take"]
+list2=["Dear","Sir"]
+list3=[x + y for x in list1 for y in list2]
+print(list3)
